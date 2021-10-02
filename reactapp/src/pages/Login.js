1c8be801ad1ab039db07211be8b69825fd9b2872
harish-dalal <harishdalal76@gmail.com>
2021-10-02 19:06:30 +0000

    loging page coding started

--- reactapp/src/pages/Login.js
+++ reactapp/src/pages/Login.js
@@ -5,27 +5,39 @@ function Login() {
     
     const [state, dispatch] = useStateValue();
 
-    return (
-        <div>
-            <h2>Login Page</h2>
-            <br/>
-            <button onClick={() => dispatch({
-                type: actionTypes.SET_USER,
-                user: "John Doe",
-                userType: "user"
-            })}>Login as User</button>
+    // return (
+    //     <div>
+    //         <h2>Login Page</h2>
+    //         <br/>
+    //         <button onClick={() => dispatch({
+    //             type: actionTypes.SET_USER,
+    //             user: "John Doe",
+    //             userType: "user"
+    //         })}>Login as User</button>
             
-            <button onClick={() => dispatch({
-                type: actionTypes.SET_USER,
-                user: "John Doe",
-                userType: "admin"
-            })}>Login as Admin</button>
+    //         <button onClick={() => dispatch({
+    //             type: actionTypes.SET_USER,
+    //             user: "John Doe",
+    //             userType: "admin"
+    //         })}>Login as Admin</button>
+    //     </div>
+    // )
+
+    return(
+        <div style={{display : 'flex' , justifyContent : 'center' , alignItems : 'center'}}>
+            <div style={{display : 'flex' , flexDirection : 'column'}}>
+                <h1>LOGIN</h1>
+                <input/>
+                <input/>
+                <button>LOGIN</button>
+                <div>New to Bookstore? <a href='./signup'></a></div>
+            </div>
+            <div>
+
+            </div>
+
         </div>
     )
-
-    // return(
-
-    // )
 }
 
 export default Login;
