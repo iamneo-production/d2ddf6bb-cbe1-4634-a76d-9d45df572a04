fdfb157a0bbb3f83a7c8a0785641607f4cf4b6a4
vinay <58105215+vinay-ardhani01010@users.noreply.github.com>
2021-10-09 16:55:13 +0530

    Update ProductService.java
    product service added

--- springapp/src/main/java/com/examly/springapp/service/ProductService.java
+++ springapp/src/main/java/com/examly/springapp/service/ProductService.java
@@ -1,3 +1,45 @@
-public class ProductService {
-    
+package com.examly.springapp;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.examly.springapp.service.ProductService;
+import com.examly.springapp.model.ProductModel;
+import java.util.List;
+
+
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping("/products")
+    public List<ProductModel>  getAllProducts()
+    {
+        return productService.getAllProducts();
+    }
+
+    @RequestMapping("/product/{id}")
+    public ProductModel getProduct(@PathVariable String id)
+    {
+        return productService.getProduct();
+    }
+
+    @PostMapping("/addProduct")
+    public ProductModel addProduct(@RequestBody ProductModel newProduct)
+    {
+        return productService.addProduct(newProduct);
+    }
+
+    @RequestMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable String id)
+    {
+        return productService.deleteProduct(id);
+    }
+
 }
