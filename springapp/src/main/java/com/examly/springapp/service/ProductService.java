07c0c323ee5945392565114f09abbc9a8d8e3482
vinay <58105215+vinay-ardhani01010@users.noreply.github.com>
2021-10-09 17:27:50 +0530

    Update ProductService.java
    Product service

--- springapp/src/main/java/com/examly/springapp/service/ProductService.java
+++ springapp/src/main/java/com/examly/springapp/service/ProductService.java
@@ -1,45 +1,33 @@
 package com.examly.springapp;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import com.examly.springapp.service.ProductService;
+import com.examly.springapp.repository.ProductRepository;
 import com.examly.springapp.model.ProductModel;
-import java.util.List;
+import java.util.*;
 
-
-
-@RestController
-public class ProductController {
-
+@Service
+public class ProductService {
     @Autowired
-    private ProductService productService;
+    private ProductRepository productRepository;
 
-    @RequestMapping("/products")
-    public List<ProductModel>  getAllProducts()
+    public List<ProductModel> getAllProducts()
     {
-        return productService.getAllProducts();
+        //db work;
+    }
+    public ProductModel getProduct(String id)
+    {
+       //dbwork
+    }
+    public String deleteProduct(String id)
+    {
+        //db -work;
+        return "deleted";
+    }
+    public ProductModel addProduct(ProductModel product)
+    {
+        //Dbwork;
+        
     }
 
-    @RequestMapping("/product/{id}")
-    public ProductModel getProduct(@PathVariable String id)
-    {
-        return productService.getProduct();
-    }
-
-    @PostMapping("/addProduct")
-    public ProductModel addProduct(@RequestBody ProductModel newProduct)
-    {
-        return productService.addProduct(newProduct);
-    }
-
-    @RequestMapping("/product/delete/{id}")
-    public String deleteProduct(@PathVariable String id)
-    {
-        return productService.deleteProduct(id);
-    }
 
 }
