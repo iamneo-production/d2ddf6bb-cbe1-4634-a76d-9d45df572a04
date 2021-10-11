69e75a68beb927fb898dfd57aa0191aa44172a9c
vinay-ardhani01010 <ardhanivinay100@gmail.com>
2021-10-11 11:02:12 +0000

    routes-done

--- springapp/src/main/java/com/examly/springapp/controller/ProductController.java
+++ springapp/src/main/java/com/examly/springapp/controller/ProductController.java
@@ -1,16 +1,46 @@
-package com.examly.springapp;
+package com.examly.springapp.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.examly.springapp.service.ProductService;
+import com.examly.springapp.model.ProductModel;
+import java.util.List;
+
+
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
     @RequestMapping("/products")
-    public String  showProducts()
+    public List<ProductModel>  getAllProducts()
     {
-        return "all products";
+        return productService.getAllProducts();
     }
+
+    @RequestMapping("/product/{id}")
+    public ProductModel getProduct(@PathVariable String id)
+    {
+        
+        return productService.getProduct(id);
+    }
+
+    @PostMapping("/addProduct")
+    public ProductModel addProduct(@RequestBody ProductModel newProduct)
+    {   System.out.println(newProduct);
+        return productService.addProduct(newProduct);
+    }
+
+    @RequestMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable String id)
+    {
+        return productService.deleteProduct(id);
+    }
+
 }
--- springapp/src/main/java/com/examly/springapp/service/ProductService.java
+++ springapp/src/main/java/com/examly/springapp/service/ProductService.java
@@ -1,3 +1,44 @@
+package com.examly.springapp.service;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.examly.springapp.repository.ProductRepository;
+import com.examly.springapp.model.ProductModel;
+import java.util.*;
+
+@Service
 public class ProductService {
-    
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<ProductModel> getAllProducts()
+    {
+        var it = productRepository.findAll();
+
+        var users = new ArrayList<ProductModel>();
+        it.forEach(e -> users.add(e));
+
+        return users;
+    }
+    public ProductModel getProduct(String id)
+    {
+       //dbwork
+       
+       ProductModel prod = productRepository.findById(id).get();
+       return prod;
+
+    }
+    public String deleteProduct(String id)
+    {
+        //db -work;
+        return "deleted";
+    }
+    public ProductModel addProduct(ProductModel product)
+    {
+       
+        
+        productRepository.save(new ProductModel("1234","abcd","vivo","!200","xyz","1"));
+        return product;      
+    }
+
+
 }
