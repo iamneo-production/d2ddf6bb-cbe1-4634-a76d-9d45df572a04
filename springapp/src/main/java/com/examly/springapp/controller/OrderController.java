a8cd1c7ea42d9e22a0f946d36d0af5d267f88bb3
abhi4mu <abhinavmunagala4@gmail.com>
2021-10-11 12:44:29 +0000

    cart no error

--- springapp/src/main/java/com/examly/springapp/controller/OrderController.java
+++ springapp/src/main/java/com/examly/springapp/controller/OrderController.java
@@ -7,38 +7,38 @@ import java.util.*;
 import com.examly.springapp.model.OrderModel;
 import com.examly.springapp.service.OrderService;
 
-@RestController
+// @RestController
 public class OrderController {
 
-    @Autowired
-    private OrderService orderService;
-    /*
-    i. List<OrderTemp> getUserProducts(String id): This method helps to
-    list the orders based on the user id.
-    ii. saveProduct(String id): This method helps to save the cart items as an
-    order.
-    iii. placeOrder(OrderModel order): This method helps to place an order
-    by the customer.
-     */
+    // @Autowired
+    // private OrderService orderService;
+    // /*
+    // i. List<OrderTemp> getUserProducts(String id): This method helps to
+    // list the orders based on the user id.
+    // ii. saveProduct(String id): This method helps to save the cart items as an
+    // order.
+    // iii. placeOrder(OrderModel order): This method helps to place an order
+    // by the customer.
+    //  */
 
-    @RequestMapping(method=RequestMethod.GET, value="/orders")
-    public List<OrderModel> getUserProducts(@RequestBody String id) {
-        return orderService.getUserProducts(id);
-    }
+    // @RequestMapping(method=RequestMethod.GET, value="/orders")
+    // public List<OrderModel> getUserProducts(@RequestBody String id) {
+    //     return orderService.getUserProducts(id);
+    // }
 
-    /*
-        Assuming the id is userId
-     */
-    @RequestMapping(method=RequestMethod.POST, value="/saveOrder")
-    public ResponseEntity<String> saveProduct(@RequestBody String id) {
-        return orderService.saveProduct(id);
-    }
+    // /*
+    //     Assuming the id is userId
+    //  */
+    // @RequestMapping(method=RequestMethod.POST, value="/saveOrder")
+    // public ResponseEntity<String> saveProduct(@RequestBody String id) {
+    //     return orderService.saveProduct(id);
+    // }
     
-    /*
-        need product id, to update quantity, maybe pass by PathVariable
-     */
-    @RequestMapping(method=RequestMethod.POST, value="/placeOrder")
-    public ResponseEntity<String> placeOrder(@RequestBody OrderModel order) {
-        return orderService.placeOrder(order);
-    }
+    // /*
+    //     need product id, to update quantity, maybe pass by PathVariable
+    //  */
+    // @RequestMapping(method=RequestMethod.POST, value="/placeOrder")
+    // public ResponseEntity<String> placeOrder(@RequestBody OrderModel order) {
+    //     return orderService.placeOrder(order);
+    // }
 }
