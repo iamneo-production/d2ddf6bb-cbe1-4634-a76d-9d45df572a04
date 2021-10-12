48f67e540c3b0d0fd6de8613927bd3c64dcffc28
abhi4mu <abhinavmunagala4@gmail.com>
2021-10-12 10:49:56 +0000

    signup

--- springapp/src/main/java/com/examly/springapp/controller/SignupController.java
+++ springapp/src/main/java/com/examly/springapp/controller/SignupController.java
@@ -1,3 +1,24 @@
+package com.examly.springapp.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import com.examly.springapp.service.SignupService;
+import com.examly.springapp.model.UserModel;
+import java.util.List;
+
+@RestController
 public class SignupController {
     
+    @Autowired
+    private SignupService signupService;
+
+    @PostMapping("/signup")
+    public void saveUser(@RequestBody UserModel user){
+        if(!signupService.isUserPresent(user.getEmail()));
+            signupService.saveUser(user);
+    }
 }
--- springapp/src/main/java/com/examly/springapp/service/SignupService.java
+++ springapp/src/main/java/com/examly/springapp/service/SignupService.java
@@ -1,3 +1,23 @@
+package com.examly.springapp.service;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.examly.springapp.repository.SignupRepository;
+import com.examly.springapp.model.UserModel;
+import java.util.*;
+
+@Service
 public class SignupService {
-    
+    @Autowired
+    private SignupRepository signupRepository;
+
+    /*this can be used to check whether a user with same 
+    email exits or not*/
+    public Boolean isUserPresent(String email){
+        return signupRepository.existsByEmail(email);
+    }
+
+    /*inserting data*/
+    public void saveUser(UserModel user){
+        signupRepository.save(user);
+    }
 }
