ce58a31be414158dd4d306af9f97d1a3bc2e04d6
Namit-Nayan <namitnayan32@gmail.com>
2021-10-14 05:37:00 +0000

    change CartModel data type to Long in CartRepository.

--- springapp/src/main/java/com/examly/springapp/repository/CartRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/CartRepository.java
@@ -1,10 +1,11 @@
 package com.examly.springapp.repository;
 
+
 import org.springframework.data.repository.CrudRepository;
 import com.examly.springapp.model.CartModel;
 import java.util.List;
 
-public interface CartRepository extends CrudRepository<CartModel, String> {
+public interface CartRepository extends CrudRepository<CartModel, Long> {
 
     //findBy__
     List<CartModel> findAllByUserId(Long userId);
